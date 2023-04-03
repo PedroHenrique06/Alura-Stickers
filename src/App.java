1fd450feca0d1817b1f1b614a413037565372ccd
Pedro Henrique <pedrosales2806@gmail.com>
2023-04-03 19:13:48 -0300

    Refatoração de código para criação de novas classes

--- src/App.java
+++ src/App.java
@@ -1,31 +1,27 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // Fazer a conexão HTTP e buscar a lista de filmes
-        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-        URI address = URI.create(url);
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder(address).GET().build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        String body = response.body();
+        // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+       //  String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2023-04-02&end_date=2023-04-03";
+       
+       String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2023-04-01";
+
+        HTTPClient http = new HTTPClient();
+        String json = http.seachData(url);
 
         // Extrair os dados e inserir numa estrutura de dados
         JsonParser parser = new JsonParser();
-        List<Map<String, String>> listaDeFilme = parser.parse(body);
+        List<Map<String, String>> listOfContent = parser.parse(json);
 
         // Manipular e exibir os filmes
-        for (Map<String,String> filme : listaDeFilme) {
-            String urlImage = filme.get("image");
-            String title = filme.get("title");
+        for (Map<String,String> content : listOfContent) {
+            String urlImage = content.get("url");
+            String title = content.get("title");
             
             InputStream inputStream = new URL(urlImage).openStream();
             
