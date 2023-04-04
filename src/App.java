88ce7b44df1f180b4d784a88d63b55ee51aa97e8
Pedro Henrique <pedrosales2806@gmail.com>
2023-04-03 21:38:52 -0300

    Refatoração do código e adição do conceito de interface

--- src/App.java
+++ src/App.java
@@ -1,36 +1,36 @@
 import java.io.InputStream;
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        // Fazer a conexão HTTP e buscar a lista de filmes
+    public static void main(String[] args) throws Exception {  
+
         // String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-       //  String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2023-04-02&end_date=2023-04-03";
-       
-       String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2023-04-01";
+
+        // Extrator para a API da IMDB
+        // ContentExtractor extractor = new IMDBContentExtractor();
+        
+        // Fazer a conexão HTTP e buscar a lista de conteúdos
+        String url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2023-03-28&end_date=2023-04-03";
+
+        // Extrator para a API da NASA
+        ContentExtractor extractor = new NasaContentExtractor();
 
         HTTPClient http = new HTTPClient();
         String json = http.seachData(url);
 
-        // Extrair os dados e inserir numa estrutura de dados
-        JsonParser parser = new JsonParser();
-        List<Map<String, String>> listOfContent = parser.parse(json);
+        // Lista de conteúdos extraidos
+        List<Content> listOfContent = extractor.contentExtractor(json);
 
         // Manipular e exibir os filmes
-        for (Map<String,String> content : listOfContent) {
-            String urlImage = content.get("url");
-            String title = content.get("title");
+        for (Content content : listOfContent) {            
+            InputStream inputStream = new URL(content.getUrlImage()).openStream();
+            String filename = "output/" + content.getTitle() + ".png"; 
             
-            InputStream inputStream = new URL(urlImage).openStream();
-            
-            String filename = "output/" + title + ".png"; 
             StickerGenerator generator = new StickerGenerator();
-            generator.create(inputStream, filename);
+            generator.create(inputStream, filename, content.getTitle());
 
-            System.out.println(title);
-
+            System.out.println(content.getTitle());
         }
     }
 }
